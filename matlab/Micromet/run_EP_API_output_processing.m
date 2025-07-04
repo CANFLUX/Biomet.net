179b7fe6326103075a1420d8e3640e2820d48e02
znesic <z_nesic@hotmail.com>
2025-07-04 15:51:04 -0700

    Adding more comments to run_EP_API_output_processing

--- matlab/Micromet/run_EP_API_output_processing.m
+++ matlab/Micromet/run_EP_API_output_processing.m
@@ -1,19 +1,38 @@
 function run_EP_API_output_processing(dateIn,siteID,epapiPath)
+%  run_EP_API_output_processing - move EddyPro API outputs to siteID\Flux folder
+% 
+% Inputs:
+%   dateIn              - date of recalculations (folder where EddyPro API output files are saved)
+%                         datetime string or a datenum
+%   siteID              - site ID (name)
+%   epapiPath           - path where EddyPro API stores siteID output files
 %
-% This function transfers EddyPro API (EPAPI) outputs from EPAPI folder to 
+%
+% This function transfers EddyPro API (EPAPI) outputs from the EPAPI folder to 
 %  Sites\siteID\Flux folder
 %
-% File locations
-%   EPAPI outputs:                  apapiPath\siteID\eddyProAPIOutputs\siteID\yyyymmddhhmm
-%   renamed EP files:               Sites\siteID\Flux
 %   
-
-% for a given subfolder name, find all _full_output_ files and copy them to
-% "Sites\Common\EddyPro_Output\siteID\full_output_from_auto_processing_for_Zoran" 
-% folder
-% (this will be automated later, the program
-% will search for the new/unprocessed data folders and process them
-% automatically)
+% Example:
+%     dateIn = "Jun 30, 2025";
+%     siteID = 'OHM';
+%     epapiPath = '\\137.82.55.154\highfreq';
+%     run_EP_API_output_processing(dateIn,siteID,epapiPath)
+%  The function will search inside this folder: \\137.82.55.154\highfreq\siteID
+%  for all the sub-folders that match this pattern: 20250603*
+%  It will then rename all the EP API output files found in those folders to
+%  the standard EP output file-name format:
+%     eddypro_siteID_startdate_enddate_full_output_recalctime_adv.csv
+%  and store those renamed files into the myProject\Sites\siteID\Flux folder
+%
+%
+%  Note: this function could be updated later so that it processes only the YYYYMMDDHHMM
+%        sub-folders that have not been processed already. 
+%
+%
+% (c) Zoran Nesic                   File created:       Jul  4, 2025
+%                                   Last modification:  Jul  4, 2025
+%
+%
 
 
 %dateIn could be a string/datetime or datenum 
@@ -23,6 +42,10 @@ if isnumeric(dateIn)
 else
     % assume is datetime or a string
     dateOfProcessing = datetime(dateIn);
+end
+
+if length(dateOfProcessing) > 1
+    error('dateIn cannot be an array!');
 end
 
 % Set the location where the properly named EP files should go 
@@ -46,6 +69,6 @@ for cntFolders = 1:length(apiOutputFolder)
         epapiFolder = fullfile(apiOutputFolder(cntFolders).folder,apiOutputFolder(cntFolders).name);
         % Rename EP API output file to the Ecoflux/Micromet standardized EP name outputs
         flagOverwrite = 0;
-        nFiles = rename_EP_API_files(siteID,epapiFolder,fluxFolder,flagOverwrite);
+        rename_EP_API_files(siteID,epapiFolder,fluxFolder,flagOverwrite);
     end
 end
