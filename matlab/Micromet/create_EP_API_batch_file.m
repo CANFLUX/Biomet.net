0f061e3473a2f40e92a6a096ecaefbc95206846b
znesic <z_nesic@hotmail.com>
2025-08-26 21:25:55 -0700

    Edits in create_EP_API_batch_file

--- matlab/Micromet/create_EP_API_batch_file.m
+++ matlab/Micromet/create_EP_API_batch_file.m
@@ -13,10 +13,22 @@ function create_EP_API_batch_file(siteID,startDay,endDay,EP_template,sourceDirMa
 %
 %
 % Zoran Nesic               File created:       Aug 14, 2025
-%                           Last modification:  Aug 14, 2025
+%                           Last modification:  Aug 26, 2025
 
 % Revisions
 %
+% Aug 26, 2025 (Zoran)
+%   - Improvements: moved more lines from main_EP_API_script.bat into 
+%     the batch file:batchFileName  created by this function. 
+
+structProject = get_TAB_project;
+ind=strfind(structProject.EPAPIpath,':');
+if isfield(structProject,'EPAPIpath') && ~isempty(ind)
+    diskName = structProject.EPAPIpath(1:ind(1));
+else
+    error('Field structProject.EPAPIpath does not exist or it does not contain disk name (c:...).');
+end
+
 
 fid = fopen(batchFileName,'w');
 if fid<0 
@@ -34,7 +46,12 @@ sourceDirEnd   = fullfile(sourceDirMain,siteID,'HighFrequencyData','raw',string(
 sourceDirEnd   = regexprep(sourceDirEnd,'\','/');
 
 fprintf(fid,'ECHO Running Matlab-generated Python call \n');
+fprintf(fid,'@%s \n',diskName);
+fwrite(fid,['@cd ' structProject.EPAPIpath newline],"uchar");
+fprintf(fid, '@CALL .venv\\Scripts\\activate \n');
+%@CALL C:\Projects\Scripts\EP_API_call.bat
 % python eddyProAPI.py --siteID UQAM_3 --biometUser True --eddyProStaticConfig Templates/ClosedPathStandard.eddypro --dateRange 2025-4-10 2025-7-21 --sourceDir U:/Sites/UQAM_3/HighFrequencyData/raw/2025 --runMode 2
 fprintf(fid,'python eddyProAPI.py --siteID %s --biometUser True --eddyProStaticConfig %s --dateRange %s %s --sourceDir %s %s --runMode 2\n',siteID,EP_template,startDay,endDay,sourceDirStart,sourceDirEnd);
+fprintf(fid,'@CALL .venv\\Scripts\\deactivate \n'); 
 fprintf(fid,'ECHO Finished with Python \n');
 fclose(fid);
