a897480c4e1c39a47f487b3600ad68d9923ea815
June-Skeeter <thruhikerflash@gmail.com>
2025-08-29 10:52:08 -0700

    updates

--- matlab/TRACEANALYSIS_FIRSTSTAGE/read_yaml_config.m
+++ matlab/TRACEANALYSIS_FIRSTSTAGE/read_yaml_config.m
@@ -10,10 +10,12 @@ fclose(fid);
 trace_str_ini
 
 
+% Since the old method only provides a fid ....
 fid = fopen(yaml_path,'rt')
+fclose(fid);
 
 
-iniFileName = char(arrayfun(@fopen, fid, 'UniformOutput', 0));
+ymlFileName = char(arrayfun(@fopen, fid, 'UniformOutput', 0));
 % Extract the ini file type ('first','second','third')
 [iniFilePath,iniFileType,~] = fileparts(iniFileName);
 [~,iniFileType,~] = fileparts(iniFileType);
@@ -26,6 +28,43 @@ elseif endsWith(iniFileType,'ThirdStage','ignorecase',true)|| endsWith(iniFileTy
     iniFileType = 'second';    
 end
 
-f = yaml.loadFile(yaml_path);
-f
-trace_str_yml = [];
+
+trace_str = [];
+
+trace_str.stage = 'none';  %stage of cleaning, used later by cleaning functions ('none' = no cleaning, 'first' = first stage cleaning, 'second' = second stage cleaning)
+trace_str.Error = 0;       %default error code, 0=no error, 1=read error
+
+trace_str.Site_name = '';
+trace_str.variableName = '';
+trace_str.ini = [];
+trace_str.SiteID = '';
+trace_str.Year = '';
+trace_str.Diff_GMT_to_local_time = '';
+trace_str.Last_Updated = '';
+trace_str.data = [];
+trace_str.DOY = [];
+trace_str.timeVector = [];
+trace_str.data_old = [];
+
+%First stage cleaning specific fields
+trace_str.stats = [];				%holds the stats about the cleaning
+trace_str.runFilter_stats = [];     %holds the stats about the filtering
+trace_str.pts_restored = [];		%holds the pts that were restored
+trace_str.pts_removed = [];		    %holds the pts that were removed
+
+%Second Stage specific fields
+trace_str.data = [];                %holds calculated data from Evalutation routine
+trace_str.searchPath = '';          %holds the options used to determine the path of the second stage data
+trace_str.input_path = '';          %holds the path of the database of the source data
+trace_str.output_path = '';         %holds the path where output data is dumped
+trace_str.high_level_path = '';
+
+% Define which fileds in the ini must exist
+required_common_ini_fields = {'variableName', 'title', 'units'};
+required_first_stage_ini_fields = {'inputFileName', 'measurementType', 'minMax'};
+required_second_stage_ini_fields = {'Evaluate1'};
+
+
+% // f = yaml.loadFile(yaml_path);
+% // f
+% // trace_str_yml = [];
