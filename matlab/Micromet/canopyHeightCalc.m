421d2ea3808fffd45b997fc75d009cd7abc0bbad
znesic <z_nesic@hotmail.com>
2026-03-12 15:12:07 -0700

    final touches for canopyHeightCalc

--- matlab/Micromet/canopyHeightCalc.m
+++ matlab/Micromet/canopyHeightCalc.m
@@ -1,9 +1,5 @@
 function canopy_height = canopyHeightCalc(measuredSensorH,WTD,wind_speed,ustar,zdL,optionsIn)
-% db_struct2database - creates a sparse database (database that does not contain all hhour values)
-%
-% eg. [structIn,dbFileNames, dbFieldNames,errCode] = ...
-%             db_struct2database(Stats,'v:\database\2023\BBS\Chambers',[],[],[],NaN);
-%       would update the database using for the year 2023.
+% canopy_height = canopyHeightCalc(measuredSensorH,WTD,wind_speed,ustar,zdL,optionsIn)
 %
 % Inputs:
 %       measuredSensorH         - EC sensor height in (meters)
@@ -16,22 +12,35 @@ function canopy_height = canopyHeightCalc(measuredSensorH,WTD,wind_speed,ustar,z
 %          .min_Ustar             - default  0.2
 %          .max_zdL               - default  0.01
 %          .max_canopyH           - default  3 m
-%          .smooth_method         - if ~empty then it's type of smooth() filtering. default: []
-%                                   other suggested option: "rloess"
-%          .smooth_span           - number of point or the fraction of all points (see "help smooth")
+%          .smooth_method         - if ~empty then it's a type of filtering. default: []
+%                                   Suggested options: "filtfilt" (recommended) ,"rloess","moving"
+%                                    - if "filtfilt" use the filtfilt() function. 
+%                                    - otherwise pass the options to  smooth() function.
+%          .smooth_span           - 60-120 works well with "filtfilt" (higher number -> more smooting)
+%                                   when using smooth(), it's the number of point or the fraction (0-1) of all points 
+%                                   (see "help smooth") 480 (default) works well with "rloess" or "moving"
+%                                   
 %
 % Outputs:
 %       canopy_height           - (filtered) canopy_height
 %
+% Example:
+%       optionsIn.smooth_method = "filtfilt";
+%       optionsIn.smooth_span = 120;
+%       canopy_height_filter = canopyHeightCalc(measuredSensorH,WTD,wind_speed,ustar,zdL,optionsIn)
+%
 %
 % NOTE: 
+%       When using "rloess" it can take a very long time to get the result (>10s compared to a few ms). 
 %
 %
 % (c) Zoran Nesic               File created:       Mar 11, 2026
-%                               Last modification:  Mar 11, 2026
+%                               Last modification:  Mar 12, 2026
 
 % Revisions:
 % 
+% Mar 12, 2026 (Zoran)
+%   - added filtfilt option
 
 defaultOptionsIn.k              = 0.4;
 defaultOptionsIn.min_Ustar      = 0.2;
@@ -62,8 +71,18 @@ canopy_height(canopy_height>optionsIn.max_canopyH) = NaN;
 
 % check if smoothing is needed
 if ~isempty(optionsIn.smooth_method)
-    %notNan = ~isnan(canopy_height);
-    %heightTmp = smooth(canopy_height(notNan),optionsIn.smooth_span,optionsIn.smooth_method);
-    canopy_height = smooth(canopy_height,optionsIn.smooth_span,optionsIn.smooth_method,'omitnan');
-    %canopy_height(notNan) = heightTmp;
+    if ~strcmpi(optionsIn.smooth_method,'filtfilt')
+        canopy_height = smooth(canopy_height,optionsIn.smooth_span,optionsIn.smooth_method,'omitnan');
+    else
+        N = defaultOptionsIn.smooth_span;
+        % start with an array of NaNs
+        canopy_height_ff = nan(size(canopy_height));
+        % run the filter on all notNan values
+        notNan = ~isnan(canopy_height);
+        indAll = (1:length(canopy_height))';
+        canopy_height_ff(notNan)= filtfilt(ones(1,N)/N,1,canopy_height(notNan));
+        % interpolate the NaN values
+        canopy_height = interp1(indAll(notNan),canopy_height_ff(notNan),indAll);
+    end
+
 end
